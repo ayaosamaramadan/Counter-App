968cf37d01efb6ae0c4c089dfe8f181690e1018b
ayaosamaramadan <ayaramadan2011@gmail.com>
2025-06-25 18:39:08 +0300

    inc points

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,72 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: PointsCounter(),
+    );
+  }
+}
+
+class PointsCounter extends StatefulWidget {
+  const PointsCounter({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _PointsCounterState createState() => _PointsCounterState();
+}
+
+class _PointsCounterState extends State<PointsCounter> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purpleAccent,
+        title: Text("Points Counter"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 50),
+            Text(
+              "Counter",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.purpleAccent,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              '$_counter',
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+                color: Colors.purpleAccent,
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _counter++;
+                });
+              },
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              ),
+              child: Text("Increment Points"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
